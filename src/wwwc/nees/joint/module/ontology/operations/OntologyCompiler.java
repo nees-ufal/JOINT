c8cde6288270ebbcba20f8dfce6bbd594b9b2e4a
williamsla <williamsalcantara@gmail.com>
2015-05-05 15:41:49 -0300

    Removed main method in OntologyCompiler

--- src/wwwc/nees/joint/module/ontology/operations/OntologyCompiler.java
+++ src/wwwc/nees/joint/module/ontology/operations/OntologyCompiler.java
@@ -1,16 +1,19 @@
 package wwwc.nees.joint.module.ontology.operations;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.rio.RDFParseException;
 import wwwc.nees.joint.compiler.model.rdf.Model;
 import wwwc.nees.joint.compiler.model.rdf.impl.LinkedHashModel;
 import wwwc.nees.joint.compiler.OWLCompiler;
 import wwwc.nees.joint.compiler.OntologyLoader;
+import wwwc.nees.joint.compiler.exceptions.ObjectStoreConfigException;
 import wwwc.nees.joint.compiler.managers.LiteralManager;
 import wwwc.nees.joint.compiler.managers.RoleMapper;
 
@@ -26,7 +29,7 @@ public class OntologyCompiler {
     // VARIABLES
     // -------------------------------------------------------------------------
     //Jar file that will be compiled the ontologies
-    private File jarFile;
+    private final File jarFile;
     //List of ontologies URLs
     private List<URL> ontologies;
     //Alibaba OWLCompiler
@@ -34,7 +37,7 @@ public class OntologyCompiler {
     //Variable to load ontologies
     private OntologyLoader loader;
     //Variable for prefixes configuration
-    private PrefixConfig prefixConfig;
+    private final PrefixConfig prefixConfig;
 
     // CONSTRUCTOR
     // -------------------------------------------------------------------------
@@ -43,11 +46,11 @@ public class OntologyCompiler {
      * desired ontologies and the ontologies to be compiled
      *
      * @param path the path to the Jar file
-     * @param urls the List of ontologies urls (either a remote or local file)
+     * @param ontologiesURLs the List of ontologies urls (either a remote or local file)
      */
     public OntologyCompiler(String path, List<String> ontologiesURLs) {
         jarFile = new File(path);
-        ontologies = new ArrayList<URL>();
+        ontologies = new ArrayList<>();
         this.setOntologies(ontologiesURLs);
         this.prepareCompiler();
         prefixConfig = new PrefixConfig(converter, loader);
@@ -83,7 +86,7 @@ public class OntologyCompiler {
 
             //Compile the specified ontologies in the desired jar file
             converter.createJar(jarFile);
-        } catch (Exception e) {
+        } catch (RepositoryException | ObjectStoreConfigException e) {
             Logger.getLogger(OntologyCompiler.class.getName()).
                     log(Level.SEVERE, null, e);
         }
@@ -126,56 +129,9 @@ public class OntologyCompiler {
             //Set the class loader
             converter.setClassLoader(cl);
 
-        } catch (Exception e) {
+        } catch (RDFParseException | IOException e) {
             Logger.getLogger(OntologyCompiler.class.getName()).
                     log(Level.SEVERE, null, e);
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println("getOntologyCompiler");
-        String path = "C:\\repositorio-meututor\\trunks\\MeuTutorAdhoc\\lib\\MeuTutorModel.jar";
-        String prefix = "file:///C:\\repositorio-meututor\\Modelagem\\Ontologies\\MeuTutor\\";
-        String foaf = prefix + "foaf.owl";
-        String sioc = prefix + "sioc.owl";
-        String cp = prefix + "MeuTutor.Domain.CP.owl";
-        String resource = prefix + "MeuTutor.Domain.Resource.owl";
-        String curriculum = prefix + "MeuTutor.Domain.Curriculum.owl";
-        String domain = prefix + "MeuTutor.Domain.owl";
-        String pedagogical = prefix + "MeuTutor.Pedagogical.owl";
-        String gamification = prefix + "MeuTutor.Gamification.owl";
-        String administrator = prefix + "MeuTutor.Administrator.owl";
-        String learner = prefix + "MeuTutor.Learner.owl";
-        String collaborative = prefix + "MeuTutor.Collaborativelearning.owl";
-        String peer = prefix + "MeuTutor.PeerAssessment.owl";
-        String adm = prefix + "MeuTutor.Administrator.owl";
-        String organization = prefix + "MeuTutor.Organization.owl";
-        String school = prefix + "MeuTutor.School.owl";
-        String teacher = prefix + "MeuTutor.Teacher.owl";
-        String coordinator = prefix + "MeuTutor.Coordinator.owl";
-        String principal = prefix + "MeuTutor.Principal.owl";
-        List<String> ontologiesURLs = new ArrayList<String>();
-        ontologiesURLs.add(foaf);
-        ontologiesURLs.add(sioc);
-        ontologiesURLs.add(cp);
-        ontologiesURLs.add(resource);
-        ontologiesURLs.add(curriculum);
-        ontologiesURLs.add(domain);
-        ontologiesURLs.add(pedagogical);
-        ontologiesURLs.add(gamification);
-        ontologiesURLs.add(administrator);
-        ontologiesURLs.add(learner);
-        ontologiesURLs.add(collaborative);
-        ontologiesURLs.add(peer);
-        ontologiesURLs.add(adm);
-        ontologiesURLs.add(organization);
-        ontologiesURLs.add(school);
-        ontologiesURLs.add(teacher);
-        ontologiesURLs.add(coordinator);
-        ontologiesURLs.add(principal);
-
-        OntologyCompiler result = new OntologyCompiler(path, ontologiesURLs);
-        result.compile();
-        System.out.println("fim");
     }
 }
